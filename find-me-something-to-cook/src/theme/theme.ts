63945b2baf6c3a87b3a40dfac58cca9c4f63407c
Gia Tran <catnguyen.tgcn@gmail.com>
2026-02-04 18:23:22 -0600

    Add theme.ts

---
+++ find-me-something-to-cook/src/theme/theme.ts
@@ -0,0 +1,31 @@
+export const theme = {
+    colors: {
+        primary: '#805436',
+        primaryLight: '#B8794F',
+        textDark: '#333333',
+        textBlack: '#000000',
+        textMuted: '#555555',
+
+        background: '#FFFef8',
+        card: '#FFFFFF',
+        stickyNote: '#FFF8D6',
+        toast: '#FFE565',
+
+        border: '#000000',
+        hover: '#888888',
+        spinnerTrack: '#F3F3F3',
+        spinnerHighlight: '#805436',
+    },
+
+    radius: {
+        small: 2,
+        medium: 5,
+        large: 10,
+    },
+
+    fonts: {
+        body: 'Courier New',
+        heading: 'Courier New',
+        mono: 'Courier New',
+    },
+};
