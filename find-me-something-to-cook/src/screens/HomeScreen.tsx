6edd761ab817dfdd795db7939cb4229596713d5b
Gia Tran <catnguyen.tgcn@gmail.com>
2026-02-05 14:12:34 -0600

    Update HomeScreen

--- find-me-something-to-cook/src/screens/HomeScreen.tsx
+++ find-me-something-to-cook/src/screens/HomeScreen.tsx
@@ -1,8 +1,15 @@
-import { View, Text, StyleSheet } from 'react-native';
-import { lightTheme, darkTheme } from "../theme/theme";
+import { useState, useCallback } from 'react';
+import { View, ScrollView, RefreshControl } from 'react-native';
 import { useTheme } from "../theme/theme";
+import { lightTheme, darkTheme } from "../theme/theme";
 import { ThemeSwitch } from '../components/ThemeSwitch';
 import SearchBar from '../components/SearchBar';
+
+import { SearchResultsSection } from '../components/SearchResultsSection';
+import { RandomRecipesSection } from '../components/RandomRecipesSection';
+import { RecentlyViewedSection } from '../components/RecentlyViewedSection';
+
+import { Recipe } from '../types';
 
 const HomeScreen = () => {
     const { theme } = useTheme();
@@ -21,6 +28,7 @@ const HomeScreen = () => {
             <SearchBar />
 
             {/* Search Results */}
+
             {/* Random Recipes */}
             {/* Recently Viewed */}
 
