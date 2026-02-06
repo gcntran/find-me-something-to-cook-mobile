7dc2bc4ea60cfd145af01aee54da9f8a157bebcf
Gia Tran <catnguyen.tgcn@gmail.com>
2026-02-05 23:18:26 -0600

    Add a profile screen

---
+++ find-me-something-to-cook/src/screens/ProfileScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+// Profile screen, this screen is for the next assignment
+export default function Profile() {
+    return (
+        <View>
+        <Text>Your Profile Settings</Text>
+        <Text>Come back here on the next assignment.</Text>
+        </View>
+    );
+    }
