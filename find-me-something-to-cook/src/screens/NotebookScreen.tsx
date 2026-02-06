6dd7a9a349b70adfde685ecba0ee991f5ac0c08a
Gia Tran <catnguyen.tgcn@gmail.com>
2026-02-05 20:21:41 -0600

    Update Notebook announcement

--- find-me-something-to-cook/src/screens/NotebookScreen.tsx
+++ find-me-something-to-cook/src/screens/NotebookScreen.tsx
@@ -1,10 +1,10 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function Notebook() {
     return (
         <View>
         <Text>Welcome to Find Me Something to Cook Notebook!</Text>
-        <Text>Here you will find your saved favorite recipes.</Text>
+        <Text>Come back here on the next assignment.</Text>
         </View>
     );
     }
